51f57487527f809b618459e2e86b389b8a68e456
Nicholas Van Beek <fastnick94@gmail.com>
2016-02-22 23:56:29 -0500

    start of fitness function

---
+++ app/controllers.js
@@ -0,0 +1,8 @@
+angular.module('controllers', [])
+.controller('test', function($scope, Genetic) {
+  var people = ['Nick', 'Zach', 'Tyler'];
+
+  var ratings = { Nick: { Zach: 2, Tyler: 2 }};
+
+  
+});
--- app/services.js
+++ app/services.js
@@ -1,11 +1,31 @@
 angular.module('services', [])
 .service('Genetic', function() {
-  var genetic = Genetic.create();
-  genetic.optimize = Genetic.Optimize.Maximize;
-  genetic.select1 = Genetic.Select1.Tournament2;
-  genetic.select2 = Genetic.Select2.Tournament2;
+  var genetic;
+  var ratings;
+  var people;
 
-  this.init = function() {
+  this.init = function(p, r) {
+    ratings = r;
+    people = p;
 
+    genetic = Genetic.create();
+    genetic.optimize = Genetic.Optimize.Maximize;
+    genetic.select1 = Genetic.Select1.Tournament2;
+    genetic.select2 = Genetic.Select2.Tournament2;
+
+    genetic.seed = function() {
+
+    }
+
+    genetic.fitness = function(chromosome) {
+      var fitness = 0;
+
+      for (var i=1; i<chromosome.length; i++) {
+        if (ratings[chromosome[i]]) {
+
+        }
+        fitness += rati
+      }
+    }
   }
 });
