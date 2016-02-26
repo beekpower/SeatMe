185e3b2fdcd06d404e0aabd5c5f3e7938ce906d1
Nicholas Van Beek <fastnick94@gmail.com>
2016-02-25 23:34:37 -0500

    ui fixes

--- app/controllers.js
+++ app/controllers.js
@@ -1,42 +1,6 @@
 angular.module('controllers', [])
-.controller('test', function($scope, Genetic) {
-  var people = ["Nick", "Tyler", "Zach"];
-
-  var relationships = [
-    { source: "Nick", target: "Tyler", relation: 2},
-    { source: "Nick", target: "Zach", relation: 2}
-  ];
-
-  var pr = [];
-
-  for (var i=0; i < people.length; i++) {
-    pr[i] = {};
-    pr[i].name = people[i];
-    pr[i].relationships = {};
-
-    //initialize all relationships to 0
-    for (var j=0; j < people.length; j++) {
-      if (i != j) {
-        pr[i].relationships[people[j]] = 0;
-      }
-    }
-
-    //Now set the relationships.
-    for (var j=0; j < relationships.length; j++) {
-      if (relationships[j].source == pr[i].name) {
-        pr[i].relationships[relationships[j].target] = relationships[j].relation;
-      }
-      if (relationships[j].target == pr[i].name) {
-        pr[i].relationships[relationships[j].source] = relationships[j].relation;
-      }
-    }
-  }
-
-  alert(JSON.stringify(pr));
-
-}).controller("geneticAlgorithm", function ($scope, Genetic) {
+.controller("geneticAlgorithm", function ($scope, Genetic) {
   Genetic.evolve();
-
 }).controller("peopleController", function ($scope, Genetic, GroupMember, Relationships) {
   $scope.people = ["Nick", "Tyler", "Zach"];
 
@@ -58,25 +22,35 @@ angular.module('controllers', [])
     $scope.people.splice(index, 1);
   }
 
-  // $scope.setRelation = function (relationship)
-  // {
-  //   for (var i = 0; i < $scope.people.length; i++)
-  //   {
-  //     if ($scope.people[i].name === relationship.source)
-  //         $scope.people[i].addRelation(relationship.relation, relationship.target);
-  //     else if ($scope.people[i].name === relationship.target)
-  //         $scope.people[i].addRelation(relationship.relation, relationship.source);
-  //   }
-  // };
-  //  $scope.setRelation({target: "tyler", source:"poop", "relation": "enemies"});
-  //  $scope.setRelation({target:"SHITHEAD", source:"poop", "relation": "lovers"});
   $scope.submit = function ()
   {
+    var pr = [];
+    for (var i=0; i < $scope.people.length; i++) {
+      pr[i] = {};
+      pr[i].name = $scope.people[i];
+      pr[i].relationships = {};
+
+      //initialize all relationships to 0
+      for (var j=0; j < $scope.people.length; j++) {
+        if (i != j) {
+          pr[i].relationships[$scope.people[j]] = 0;
+        }
+      }
+
+      //Now set the relationships.
+      for (var j=0; j < $scope.relationships.length; j++) {
+        if ($scope.relationships[j].source == pr[i].name) {
+          pr[i].relationships[$scope.relationships[j].target] = $scope.relationships[j].relation;
+        }
+        if ($scope.relationships[j].target == pr[i].name) {
+          pr[i].relationships[$scope.relationships[j].source] = $scope.relationships[j].relation;
+        }
+      }
+    }
+
+    alert(JSON.stringify(pr));
+
     Genetic.init($scope.people);
     window.location.hash = "/generator";
   }
-
-
-
-
 });
